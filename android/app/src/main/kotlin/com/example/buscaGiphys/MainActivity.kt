5c62e3e8cb46971aaacaabbbab2e2fa039532145
Geraldo Abreu <gamjsr@gmail.com>
2021-11-05 12:45:36 -0300

    all files

---
+++ android/app/src/main/kotlin/com/example/buscaGiphys/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buscaGiphys
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscaGiphys/ui/gif_page.dart';
+import 'package:buscaGiphys/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
